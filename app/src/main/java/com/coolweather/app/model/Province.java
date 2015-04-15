1bfd7fbc65d44d71389404bce915acdf16eaaa9a
han <hanxuezeng.china@gmail.com>
2015-04-16 00:13:20 +0800

    Initial commit

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,37 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by han on 15/4/11.
+ */
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+}
